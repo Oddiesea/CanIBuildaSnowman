b47a805b4f2ea77f12d9ecc4c1c2078d14345795
l <liamwiltonjones@gmail.com>
2019-11-07 20:20:50 +0000

    added styled componets

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import styled from "styled-components";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Yes!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+    isSnowing: false
+  };
+
+  MessageWrapper = styled.View`
+    text-align: center;
+  `;
+
+  MessageText = styled.Text`
+    justify-content: center;
+    font-size: 30;
+  `;
+
+  render() {
+    const { isSnowing } = this.props;
+    return (
+      <this.MessageWrapper>
+        <this.MessageText>{isSnowing ? "Yes!" : "No."}</this.MessageText>;
+      </this.MessageWrapper>
+    );
+  }
+
+  componentWillMount() {
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
